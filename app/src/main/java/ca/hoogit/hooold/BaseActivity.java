86f03baf62d172844c80f5fcf183906318a11176
Jordon de Hoog <jordon.dehoog@gmail.com>
2015-08-10 21:20:31 -0400

    Some finishing touches

---
+++ app/src/main/java/ca/hoogit/hooold/BaseActivity.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (C) 2015, Jordon de Hoog
+ * <p/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package ca.hoogit.hooold;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * @author jordon
+ *         <p/>
+ *         Date    10/08/15
+ *         Description
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Bind(R.id.appBar) Toolbar mToolbar;
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResource());
+        ButterKnife.bind(this);
+        if (mToolbar != null) {
+            mToolbar.setBackgroundColor(getToolbarColor());
+            setSupportActionBar(mToolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(getDisplayHomeAsUpEnabled());
+        } else {
+            throw new NullPointerException("Layout must contain a toolbar with id 'appBar'");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (getMenuResource() != 0) {
+            getMenuInflater().inflate(getMenuResource(), menu);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    protected abstract int getToolbarColor();
+
+    protected abstract int getLayoutResource();
+
+    protected abstract int getMenuResource();
+
+    protected abstract boolean getDisplayHomeAsUpEnabled();
+
+    protected Toolbar getToolbar() {
+        return this.mToolbar;
+    }
+}
--- app/src/main/java/ca/hoogit/hooold/MainActivity.java
+++ app/src/main/java/ca/hoogit/hooold/MainActivity.java
@@ -1,42 +1,24 @@
 package ca.hoogit.hooold;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import com.crashlytics.android.Crashlytics;
-
-import io.fabric.sdk.android.Fabric;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
-        setContentView(R.layout.activity_main);
+    protected int getToolbarColor() {
+        return getResources().getColor(R.color.primary);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected int getLayoutResource() {
+        return R.layout.activity_main;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected int getMenuResource() {
+        return R.menu.menu_main;
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    @Override
+    protected boolean getDisplayHomeAsUpEnabled() {
+        return false;
     }
 }
